baaafa2af00d7d2ac3f5f483f0d143ea8fcf7549
richardm1824 <53354803+richardm1824@users.noreply.github.com>
2019-07-26 22:50:44 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# discord-spam-for-pokecord-level
+## DISCLAIMER
+This is ONLY for use on your OWN server and it is (as far as I know) within the rules of discord.
+This bot does NOT:
+* respond or react to other user's actions;respond to commands, autoreply to certain keywords, welcome users to servers, automatically react, etc. YOU are the only one that can control it.
+* 'invite scraping'
+* spam, insult or demean others, post NSFW material, spread viruses or illegal material, etc.
+
+## Features
+The bot will spam the channel with random text. This will help level your pokemon on the discord pokecord game.
+
+## Usage
+run `node bot.js` in the bash terminal.
+press Ctr + C to stop the bot from running
+
+## Current version 
+`!spam 3` will make the bot spam "<3" 9 times with 800miliseconds in between. It will then wait for 5 seconds and repeat the spam loop 3 times. NOTE: change the number 3 to any other larger number for more spams
+
+## Fixes and improvements
+* Generate random text (to replace "<3") for spamming (maybe random jokes too?)
---
+++ bot.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const config = require("./config.json");
+
+client.on("ready", () => {
+    console.log("I am ready!");
+});
+
+
+
+client.on("message", (message) => {
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if (!command || message.author.bot) return;
+    if(message.author !== client.user) return;
+  
+    if (command === "ping") {
+      message.channel.send("pong!");
+    } else
+    if (command === "spam") {
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+          }
+          async function spam() {
+            for(i=0;i<9;i++){
+                //this.self.createMessage(msg.channel.id, "<3") 
+                //lol wut
+                message.channel.send("<3");//old code
+                await sleep(800);
+            }
+          }
+          async function spamMore(){
+              for(j=0;j<args[0];j++){
+                spam();
+                await sleep(5000);
+              }
+          }
+          spamMore()
+    }
+});
+
+client.login(config.token);
